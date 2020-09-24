9ac13ca5493404dc38db3ce0feaefc953bd1864d
Arseniy Shandropol <50551178+orseniy@users.noreply.github.com>
2020-09-24 20:29:58 +0300

    Initial commit

---
+++ fin_news/spiders/yahoo_finance.py
@@ -0,0 +1,15 @@
+import scrapy
+
+class FinNewsSpider(scrapy.Spider):
+    name = 'LastNews'
+
+    start_urls = [
+        'https://finance.yahoo.com/quote/PD/news?p=PD'
+    ]
+
+    def parse(self, response):
+        for news in response.xpath("//*[@id="latestQuoteNewsStream-0-Stream"]/ul/li"):
+            yield {
+                'title' : news.xpath(".//*[@id="latestQuoteNewsStream-0-Stream"]/ul/li[4]/div/div/div[2]/h3/a/text()").get(),
+                'link' : news.xpath(".//*[@id="latestQuoteNewsStream-0-Stream"]/ul/li[4]/div/div/div[2]/h3/a/@href").get(),
+            }
