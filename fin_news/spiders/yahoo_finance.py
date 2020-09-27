9743ddca2b7f10392247d38a17593b417227c550
Arseniy Shandropol <50551178+orseniy@users.noreply.github.com>
2020-09-27 18:33:09 +0300

    no errors code

--- fin_news/spiders/yahoo_finance.py
+++ fin_news/spiders/yahoo_finance.py
@@ -1,16 +1,17 @@
 import scrapy
 
 class FinNewsSpider(scrapy.Spider):
-    name = 'Latest News'
+    name = 'Latest_News'
 
     start_urls = [
         'https://finance.yahoo.com/quote/PD/news?p=PD'
     ]
 
     def parse(self, response):
-        for news in response.xpath("//*[@id="latestQuoteNewsStream-0-Stream"]/ul/li"):
+        for news in response.xpath("//*[@id='latestQuoteNewsStream-0-Stream']/ul/li"):
             yield {
-                title = news.xpath(".//*[@id="latestQuoteNewsStream-0-Stream"]/ul/li[4]/div/div/div[2]/h3/a/text()").get(),
-                link = news.xpath(".//*[@id="latestQuoteNewsStream-0-Stream"]/ul/li[4]/div/div/div[2]/h3/a/@href").get(),
+                'title': news.xpath(".//*[@id='latestQuoteNewsStream-0-Stream']/ul/li[4]/div/div/div[2]/h3/a/text()").get(),
+                'link': news.xpath(".//*[@id='latestQuoteNewsStream-0-Stream']/ul/li[4]/div/div/div[2]/h3/a/@href").get(),
             }
+
 
