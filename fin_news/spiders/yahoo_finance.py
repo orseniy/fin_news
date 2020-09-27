855e0cb6469ba8963d44f901dcd2772976b654c1
Arseniy Shandropol <50551178+orseniy@users.noreply.github.com>
2020-09-27 20:55:40 +0300

    Added dots for selectors

--- fin_news/spiders/yahoo_finance.py
+++ fin_news/spiders/yahoo_finance.py
@@ -8,11 +8,11 @@ class FinNewsSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        for news in response.xpath("//*[@id='latestQuoteNewsStream-0-Stream']/ul/li"):
+        for news in response.xpath(".//*[@id='latestQuoteNewsStream-0-Stream']/ul/li"):
             yield {
-                'title': news.xpath("//*[@id='latestQuoteNewsStream-0-Stream']/ul/li/div/div/div/h3//text()").getall(),
-                'link': news.xpath("//*[@id='latestQuoteNewsStream-0-Stream']/ul/li/div/div/div/h3/a/@href").getall(),
+                'title': news.xpath(".//*[@id='latestQuoteNewsStream-0-Stream']/ul/li/div/div/div/h3//text()").getall(),
+                'link': news.xpath(".//*[@id='latestQuoteNewsStream-0-Stream']/ul/li/div/div/div/h3/a/@href").getall(),
             }
 
-#//*[@id='latestQuoteNewsStream-0-Stream']/ul/li/div/div/div/h3//text() - title xpath
-#//*[@id='latestQuoteNewsStream-0-Stream']/ul/li/div/div/div/h3/a/@href - link xpath
+#.//*[@id='latestQuoteNewsStream-0-Stream']/ul/li/div/div/div/h3//text() - title xpath from shell
+#.//*[@id='latestQuoteNewsStream-0-Stream']/ul/li/div/div/div/h3/a/@href - link xpath from shell
