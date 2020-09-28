4e593b8a228f8fa64915bca1e8c66e0915268ef4
Arseniy Shandropol <50551178+orseniy@users.noreply.github.com>
2020-09-28 18:02:14 +0300

    Working code (for PD example)

--- fin_news/spiders/yahoo_finance.py
+++ fin_news/spiders/yahoo_finance.py
@@ -1,5 +1,5 @@
 import scrapy
-
+from fin_news.items import FinNewsItem
 class FinNewsSpider(scrapy.Spider):
     name = 'Latest_News'
 
@@ -9,10 +9,10 @@ class FinNewsSpider(scrapy.Spider):
 
     def parse(self, response):
         for news in response.xpath(".//*[@id='latestQuoteNewsStream-0-Stream']/ul/li"):
-            yield {
-                'title': news.xpath(".//*[@id='latestQuoteNewsStream-0-Stream']/ul/li/div/div/div/h3//text()").getall(),
-                'link': news.xpath(".//*[@id='latestQuoteNewsStream-0-Stream']/ul/li/div/div/div/h3/a/@href").getall(),
-            }
+            item = FinNewsItem()
+            item['title'] = news.xpath(".//div/div/div/h3//text()").getall()
+            item['link'] = news.xpath(".//div/div/div/h3/a/@href").getall()
+            yield item
 
 #.//*[@id='latestQuoteNewsStream-0-Stream']/ul/li/div/div/div/h3//text() - title xpath from shell
 #.//*[@id='latestQuoteNewsStream-0-Stream']/ul/li/div/div/div/h3/a/@href - link xpath from shell
